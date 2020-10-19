062535adbcc08867a5e72d20c0c00b3663c6ddd2
Thomas Venken <11700525@student.pxl.be>
2020-10-19 10:43:11 +0200

    webserver escape

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -1,36 +1,36 @@
 sudo echo "
-job "webserver" {
-  datacenters = ["dc1"]
-  type = "service"
+job \"webserver\" {
+  datacenters = [\"dc1\"]
+  type = \"service\"
 
-  group "webserver" {
+  group \"webserver\" {
     count = 3
-    task "webserver" {
-      driver = "docker"
+    task \"webserver\" {
+      driver = \"docker\"
 
       config {
-        image = "httpd"
+        image = \"httpd\"
         force_pull = true
         port_map = {
           webserver_web = 80
         }
 
         logging {
-          type = "journald"
+          type = \"journald\"
           config {
-            tag = "WEBSERVER"
+            tag = \"WEBSERVER\"
           }
         }
       }
 
       service {
-        name = "webserver"
-        port = "webserver_web"
+        name = \"webserver\"
+        port = \"webserver_web\"
       }
 
       resources {
         network {
-          port "webserver_web" {}
+          port \"webserver_web\" {}
         }
       }
     }
