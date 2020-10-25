63a9015c2f1d1fff73f1acea5501a4eb1ec674bd
Thomas Venken <11700525@student.pxl.be>
2020-10-25 15:12:09 +0100

    added NOMAD_ADDR

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,3 +14,4 @@ sudo yum -y install nomad
 sudo yum -y install consul
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo sed -i '$ a export NOMAD_ADDR=http://192.168.2.15:4646' .bashrc
