06686bc898e428b4be7fe5d8b096da21a7e40c15
Thomas Venken <11700525@student.pxl.be>
2020-10-25 14:41:36 +0100

    server.sh aangepast voor gebruik sed

--- scripts/client2.sh
+++ scripts/client2.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
-
 sudo mkdir /opt/nomad/client2 > /dev/null 2>&1
-sudo echo "# Increase log verbosity
-log_level = \"DEBUG\"
-
-# Setup data dir
+sudo echo "log_level = \"DEBUG\"
 data_dir = \"/opt/nomad/client2\"
-
-# Give the agent a unique name. Defaults to hostname
 name = \"client2-nomad\"
-
-# Enable the client
+bind_addr = \"192.168.2.17\"
 client {
     enabled = true
-
-    # For demo assume we are talking to server1. For production,
-    # this should be like \"nomad.service.consul:4647\" and a system
-    # like Consul used for service discovery.
     servers = [\"192.168.2.15:4647\"]
+	network_interface=\"eth1\"
 }
-
-# Modify our port to avoid a collision with server1
 ports {
     http = 5657
 }
-
-# Disable the dangling container cleanup to avoid interaction with other clients
 plugin \"docker\" {
   config {
     gc {
@@ -34,25 +20,9 @@ plugin \"docker\" {
       }
     }
   }
-}" > /etc/nomad.d/client2.hcl
-sudo rm -f /etc/nomad.d/nomad.hcl
-
+}" > /etc/nomad.d/nomad.hcl
 sudo systemctl start nomad.service
-
-sudo echo "data_dir = \"/opt/consul\"
-
-client_addr= \"0.0.0.0\"
-
-ui = true
-
-#retry_join = [\"consul.domain.internal\"]
-retry_join = [\"192.168.2.15\"]
-#retry_join = [\"[::1]:8301\"]
-#retry_join = [\"consul.domain.internal\", \"10.0.4.67\"]
-
-bind_addr = \"192.168.2.17\"
-node_name = \"client2-consul\"
-
-
-" >> /etc/consul.d/consul.hcl
+sed -i '$ a retry_join = ["192.168.2.15"]' /etc/consul.d/consul.hcl
+sed -i '$ a bind_addr = "192.168.2.17"' /etc/consul.d/consul.hcl
+sed -i '$ a node_name = "client2-consul"' /etc/consul.d/consul.hcl
 sudo systemctl start consul.service
--- scripts/server.sh
+++ scripts/server.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
 sudo mkdir /opt/nomad/server > /dev/null 2>&1
-sudo echo "# Increase log verbosity
-log_level = \"DEBUG\"
-# Set server ip
-bind_addr = \"192.168.2.15\"
-
-# Setup data dir
-data_dir = \"/opt/nomad/server\"
-
-# Enable the server
-server {
-    enabled = true
-
-    # Self-elect, should be 3 or 5 for production
-    bootstrap_expect = 1
-}" >  /etc/nomad.d/server.hcl
-sudo rm -f /etc/nomad.d/nomad.hcl
+sed -i '2s/^/log_level = "DEBUG"/' /etc/nomad.d/nomad.hcl
+sed -i 's/0.0.0.0/192.168.2.15/g' /etc/nomad.d/nomad.hcl
+sed -i 's/\/data/\/server/g' /etc/nomad.d/nomad.hcl
+sed -i 's/127.0.0.1/192.168.2.15/g' /etc/nomad.d/nomad.hcl
 sudo systemctl start nomad.service
-sudo echo "data_dir = \"/opt/consul\"
-client_addr= \"0.0.0.0\"
-ui = true
-server = true
-bootstrap_expect=1
-#retry_join = [\"consul.domain.internal\"]
-#retry_join = [\"10.0.4.67\"]
-#retry_join = [\"[::1]:8301\"]
-#retry_join = [\"consul.domain.internal\", \"10.0.4.67\"]
-bind_addr = \"192.168.2.15\"
-" >> /etc/consul.d/consul.hcl
+sed -i 's/#server = true/server = true/g' /etc/consul.d/consul.hcl
+sed -i 's/#bootstrap_expect=3/bootstrap_expect=1/g' /etc/consul.d/consul.hcl
+sed -i '$ a bind_addr = "192.168.2.15"' /etc/consul.d/consul.hcl
 sudo systemctl start consul.service
